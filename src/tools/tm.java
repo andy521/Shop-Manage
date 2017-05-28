9512812a98b5dc7253c43108251e00fc0485c7bf
naah <nayan3480232@163.com>
2017-05-28 16:59:11 +0800

    2017年5月28日16:59:09

--- src/tools/tm.java
+++ src/tools/tm.java
@@ -1,11 +1,15 @@
 package tools;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import net.sf.json.JSONObject;
 import obj.Goods;
 
 import org.json.JSONException;
@@ -13,40 +17,25 @@ import org.json.JSONException;
 public class tm
 {
 
-	public static String loadJson(String id)
+	public static JSONObject loadJson(String id)
 	{
-		String geturl = "http://123.206.28.198/ANCC/info?username=&key=&id=";
+		String geturl = "http://123.206.28.198/ANCC/info?username=&key=&id=" + id;
 		StringBuilder json = new StringBuilder();
 		try
 		{
-			URL urlObject = new URL(geturl + id);
-			URLConnection uc = urlObject.openConnection();
-			uc.setRequestProperty("Host", "search.anccnet.com");
-			uc.setRequestProperty("Referer", geturl + id);
 
-			// 读取响应  
-			int length = (int) uc.getContentLength();// 获取长度  
-			InputStream is = uc.getInputStream();
-			if (length != -1)
+			URL urlObject = new URL(geturl);
+			HttpURLConnection uc = (HttpURLConnection) urlObject.openConnection();
+			BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream(), "utf-8"));
+			String inputLine = null;
+			while ((inputLine = in.readLine()) != null)
 			{
-				byte[] data = new byte[length];
-				byte[] temp = new byte[512];
-				int readLen = 0;
-				int destPos = 0;
-				while ((readLen = is.read(temp)) > 0)
-				{
-					System.arraycopy(temp, 0, data, destPos, readLen);
-					destPos += readLen;
-				}
-				String result = new String(data, "gb2312"); // utf-8编码  
-				return result;
+				json.append(inputLine);
 			}
-			//            BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream(),"utf-8")); 
-			//            String inputLine = null;  
-			//            while ( (inputLine = in.readLine()) != null) {  
-			//                json.append(inputLine);  
-			//            }  
-			//            in.close();  
+			in.close();
+			JSONObject jsonObject = JSONObject.fromObject(json.toString());
+			return jsonObject;
+			//				System.out.println(jsonObject);
 		}
 		catch (MalformedURLException e)
 		{
@@ -56,83 +45,37 @@ public class tm
 		{
 			e.printStackTrace();
 		}
-		return json.toString();
+		return null;
+
 	}
 
 	public static Goods getTm(String id)
 	{
 		Goods goods = new Goods();
-		String tm = id;
-		String sb = "";
-		String cj = "";
-		String mc = "";
-		String ggxh = "";
-		String ms = "";
-		int beginIx = 0;
-		int endIx = 0;
-		String json = loadJson(id).replaceAll("\\s*", "");
+		JSONObject json = loadJson(id);
 		try
 		{
 
-			beginIx = json.indexOf("<dt>商标：</dt><dd>");
-			endIx = json.indexOf("</dd><dt>发布厂家：</dt>");
-			sb = json.substring(beginIx + 16, endIx);
-
-			beginIx = json.indexOf("mLink\"target=\"_blank\">");
-			endIx = json.indexOf("</a></dd></dl><dlclass=\"p-info\"><dt>商品条码：");
-			cj = json.substring(beginIx + 22, endIx);
-
-			beginIx = json.indexOf("<dt>名称：</dt><dd>");
-			endIx = json.indexOf("</dd><dt>规格型号：</dt>");
-			mc = json.substring(beginIx + 16, endIx);
-
-			beginIx = json.indexOf("<dt>规格型号：</dt><dd>");
-			endIx = json.indexOf("</dd><dt>描述：</dt>");
-			ggxh = json.substring(beginIx + 18, endIx);
-
-			beginIx = json.indexOf("<dt>描述：</dt><dd>");
-			endIx = json.indexOf("</dd></dl><brclear=\"all\"/");
-			ms = json.substring(beginIx + 16, endIx);
+			goods.setName(json.getJSONObject("d").getString("ItemName"));
+			goods.setIdcard(id);
+			goods.setFactory(json.getJSONObject("d").getString("FirmAddress"));
+			goods.setSavetime(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
+			goods.setBounds(json.getJSONObject("d").getString("BrandName"));
+			goods.setWhight(json.getJSONObject("d").getString("ItemSpecification") + " " + json.getJSONObject("d").getString("ItemWidth") + " "
+					+ json.getJSONObject("d").getString("ItemHeight"));
 		}
 		catch (Exception e)
 		{
 			// TODO Auto-generated catch block
-			if (json.indexOf("暂时没有您要找的商品！") == -1)
-			{
-				beginIx = json.indexOf("企业名称（中文）：</td><td><spanid=\"firm_name\">") + 38;
-				endIx = json.indexOf("</span></td></tr><tr><td>企业名称（英文）");
-				mc = "目前没有该产品信息！请添加！";
-				cj = json.substring(beginIx, endIx);
-			}
-			else
-			{
-				mc = "目前没有该产品信息！请添加！";
-			}
-
 		}
 		finally
 		{
-			goods.setGoodsid(tm);
-			goods.setIdcard(tm);
-			goods.setBounds(sb);
-			goods.setFactory(cj);
-			goods.setName(mc);
-			goods.setWhight(ggxh);
-			goods.setNote(ms);
 		}
 		return goods;
 	}
 
 	public static void main(String[] args) throws JSONException
 	{
-		// TODO Auto-generated method stub
-		Goods aGoods = getTm("6900000000010");
-		System.out.println("0:" + aGoods.getGoodsid());
-		System.out.println("1:" + aGoods.getBounds());
-		System.out.println("2:" + aGoods.getFactory());
-		System.out.println("3:" + aGoods.getIdcard());
-		System.out.println("4:" + aGoods.getName());
-		System.out.println("5:" + aGoods.getNote());
-		System.out.println("6:" + aGoods.getWhight());
+
 	}
 }
